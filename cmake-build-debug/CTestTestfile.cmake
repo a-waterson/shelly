4ddd476e8a94faa813e64893dd7fadc7e0f9344e
waterman <76515579+a-waterson@users.noreply.github.com>
2022-05-22 18:27:42 -0700

    Delete cmake-build-debug directory

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aidanw/work/4980/shell
-# Build directory: /home/aidanw/work/4980/shell/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
--- cmake-build-debug/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aidanw/work/4980/shell/src
-# Build directory: /home/aidanw/work/4980/shell/cmake-build-debug/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
